59eb4f3cfaeb220457d1a7df5bccc70cb80ab548
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2022-02-20 21:35:04 +0000

    Create CppTraining.cpp

---
+++ CppTraining.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  cout << "Hello World!"\n;
+  cout << "My name is Wojtek." << endl;
+  cout << "Today is the 20th of February 2022.";
+  cout << "Have a nice day! Much love!"
+    
+  system("paused>0");
+}
