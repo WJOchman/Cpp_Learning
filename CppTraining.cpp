7265ccd24c02439bd60fed4775265c1e1d95fb78
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2022-02-21 21:19:32 +0000

    Update CppTraining.cpp

--- CppTraining.cpp
+++ CppTraining.cpp
@@ -8,3 +8,31 @@ int main() {
     
   system("paused>0");
 }
+---------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // BMI calculator
+    // weight(kg) / height * height(m)
+    // Underweight < 18.5
+    // Normal weight - 18.5 - 24.9
+    // Overweight > 25
+
+    float weight, height, bmi;
+    cout << "Weight(kg), height(m)";
+    cin >> weight >> height;
+    bmi = weight / (height * height);
+	
+    if (bmi < 18.5)
+    	cout << "You are underweight" << endl;
+    else if (bmi > 25)
+  	  cout << "You are overweight" << endl; 
+    else
+  	  cout << "You are normal weight" << endl;		
+	
+    cout << "Your BMI is" << bmi;
+
+    system("pause>0");
+}
