9ed79977c57c6824e370c52eb6c102b933631faa
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2022-03-05 08:40:43 +0000

    Update CppTraining.cpp

--- CppTraining.cpp
+++ CppTraining.cpp
@@ -213,3 +213,18 @@ void main()
 
 	system("paused>0");
 }
+---------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	for (int i = 1; i < 11; i++) {
+		for (int j = 1; j < 11; j++) {
+			cout << i << "*" << j << "=" << i * j << endl;
+		}
+		cout << endl;
+	}
+
+system("paused>0");
+}
