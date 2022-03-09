bf10e0c21d2ee025a058794aeea91e158d8a3d78
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2022-03-09 08:39:21 +0000

    Update CppTraining.cpp

--- CppTraining.cpp
+++ CppTraining.cpp
@@ -274,3 +274,26 @@ void main()
 
 system("paused>0");
 }
+---------------------------------------------------------------------------------------
+#include <iostream>
+using namespace std;
+
+void main() {
+	int userPin = 1234, pin, errorCounter = 0;
+	
+	do {
+		cout << "PIN: ";
+		cin >> pin;
+		if (pin != usersPin)
+			errorCounter++;
+
+	} while (errorCounter < 3 && pin != usersPin);
+
+	if (errorCounter < 3)
+		cout << "Loading....\n";
+		cout << "Correct!";
+	else 
+		cout << "Incorrect PIN. Blocked!";
+
+system("paused>0");
+}
